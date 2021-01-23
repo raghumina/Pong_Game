6ece454a960ded471685e539f3b2f6fac4fe6ff2
raghu <raghumina11@gmail.com>
2021-01-23 19:29:55 +0530

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -2,55 +2,92 @@
 # Pong game in Python using Pycharm module
 #
 # Lets Start
-
 import turtle
+import os
 
 wn = turtle.Screen()
+wn.title("Pong")
 wn.bgcolor("black")
-wn.title("Pong by Raghu Mina")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-# Player B/ Paddle B
-paddle_b = turtle.Turtle()
-paddle_b.speed(0)
-paddle_b.shape("square")
-paddle_b.color("blue")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
-paddle_b.penup()
-paddle_b.goto(-350, 0)
+# Score
+score_a = 0
+score_b = 0
 
-# Player A/ Paddle A
+# Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
-paddle_a.color("blue")
+paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
-paddle_a.goto(350, 0)
+paddle_a.goto(-350, 0)
 
-# bALL
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
 ball = turtle.Turtle()
 ball.speed(0)
-ball.shape("circle")
+ball.shape("square")
 ball.color("white")
-ball.shapesize(stretch_wid=1, stretch_len=1)
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 
-# Functions for movement of ball and Paddle A and Paddle B
-
+# Functions
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
     paddle_a.sety(y)
 
 
-# Keyboard binding
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+# Keyboard bindings
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
-# Main gaming loop
+# Main game loop
 while True:
     wn.update()
+
+    # Move the ball
