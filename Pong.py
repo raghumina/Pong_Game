d5fbb4e58f75459898b133d6badd45fc6c6c5425
raghu <raghumina11@gmail.com>
2021-01-23 19:07:28 +0530

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -7,16 +7,30 @@ import turtle
 wn = turtle.Screen()
 wn.bgcolor("red")
 wn.title("Pong by Raghu Mina")
-wn.setup(width= 400, height= 400)
+wn.setup(width= 800, height= 600)
 wn.tracer(0)
 
 
 # Player A/ Paddle A
-
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("blue")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
 
 
 # Player B/ Paddle B
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("blue")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(350, 0)
+
 # bALL
 
 
