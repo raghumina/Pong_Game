a65638a37eef259508faae9eec29819150fee26b
raghu <raghumina11@gmail.com>
2021-01-23 18:59:06 +0530

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -4,5 +4,23 @@
 # Lets Start
 
 import turtle
-wn = turtle.screen()
+wn = turtle.Screen()
+wn.bgcolor("red")
 wn.title("Pong by Raghu Mina")
+wn.setup(width= 400, height= 400)
+wn.tracer(0)
+
+
+# Player A/ Paddle A
+
+
+
+
+# Player B/ Paddle B
+# bALL
+
+
+
+# Main gaming loop
+while True:
+    wn.update()
